1c8c4dd7de9b71764849d7cdeb12e1065b3db3e3
Malena Andrade <malena.andrade@gmail.com>
2012-10-05 16:35:54 -0400

    Third phase, added bike parts section and image

--- js/scratch.js
+++ js/scratch.js
@@ -1,24 +1,24 @@
 $(document).ready(function(){
 
+    var biker = $('#biker');
+    var intro = $('#introduction');
+    var container = $('#container');
+    var modal = $('#second');
+    var viewPortHeight = $(document).height();
+    var viewPortWidth = $(window).width();
+    var first = $('#first');
+    var landingVerticalPosition = viewPortHeight - 200;
+    var newTopPosition = '+=' + landingVerticalPosition + 'px';
+    var landingHorizontalPosition = viewPortWidth;
+    var newLeftPosition = '+=' + landingHorizontalPosition + 'px';
+    var display = $('.display');
+    var bikeImage = $('img');
+
 	var startBiker = function() {
-		var viewPortHeight = $(document).height();
-		var viewPortWidth = $(window).width();
-
-		var landingVerticalPosition = viewPortHeight - 200;
-		var newTopPosition = '+=' + landingVerticalPosition + 'px';
-
-		var landingHorizontalPosition = viewPortWidth;
-		var newLeftPosition = '+=' + landingHorizontalPosition + 'px';
-
-		var biker = $('#biker');
 		TweenLite.to(biker, 3, {css:{top:newTopPosition, left:newLeftPosition}});
-
 	};
 
 	var landBiker = function() {
-		var biker = $('#biker');
-		var intro = $('#introduction');
-
 		TweenLite.to(biker, 2, {css:{rotation:"-=.5rad"}, ease:Power0.easeIn, onComplete:nudgeBike});
 
 		function nudgeBike(){
@@ -29,29 +29,28 @@ $(document).ready(function(){
 			TweenLite.to(biker, 2.5, {css:{left:"+=300px"}, ease:Power3.easeOut});
 			TweenLite.to(intro, 2.5, {css:{display: "block", opacity: '1'}});
 		}
-
 	};
 
 	var moveBikerFirstPhase = function(){
-		var biker = $('#biker');
-
 		TweenLite.to(biker, 2.5, {css:{left:"-=300px"}, ease:Power3.easeInOut});
 	};
 
     var moveBikerSecondPhase = function(){
-        var biker = $('#biker');
+
         TweenLite.to(biker, 2.5, {css:{left:"+=600px", opacity: 0}, ease:Power3.easeInOut, onComplete:openModal});
 
         function openModal(){
-            var modal = $('#second');
             var height = $(document).height();
-            TweenLite.to(modal, 2.5, {css:{display: "block", opacity: '0.9',height: height}, ease:Expo.easeOut});
+            var width = $(window).width();
+            TweenLite.to(modal, 2.5, {css:{display: "block", opacity: '0.9',height: height, width: width}, ease:Expo.easeOut, onComplete:openDisplay});
         }
 
+        function openDisplay(){
+            TweenLite.to(display, 2.5, {css:{display: "block", opacity: '0.9'}, ease:Expo.easeOut});
+        }
     };
 
 	var moveBackground = function(){
-		var container = $('#container');
 		TweenLite.to(container, 6, {css:{backgroundPosition:"-=800px bottom"}, ease:Strong.easeOut});
 	};
 
@@ -67,25 +66,19 @@ $(document).ready(function(){
             var newTop = Math.floor(Math.random()*500)*((i%2)?1:-1);
             var newLeft = Math.floor(Math.random()*500)*((i%2)?1:-1);
 
-            /* Tweenlite */
             $(this).css({position: "relative", opacity: 1, fontSize: 60, top: 0, left: 0});
             TweenLite.to(this, 1, {css:{opacity:"0", fontSize:"84", top: newTop, left: newLeft}});
 
         });
-
     };
 
-    var first = $('#first');
-
-	var showFirstSection = TweenLite.to(first, 1.5, {css:{display: "block", opacity: '0.8', left: '100px', width: '500px'}, ease:Expo.easeOut});
-
-    showFirstSection.pause();
+    var showFirstSection = TweenLite.to(first, 1.5, {css:{display: "block", opacity: '0.8', left: '100px', width: '500px'}, ease:Expo.easeOut});
 
 
 	startBiker();
 	landBiker();
 	moveBackground();
-
+    showFirstSection.pause();
 
     $('#continue1').on('click', function(){
 		var introduction = $('#introduction p');
@@ -101,11 +94,15 @@ $(document).ready(function(){
         moveBikerSecondPhase();
     });
 
+
+    $('#seat').on('click', function(){
+        TweenLite.to(bikeImage, 1, {css:{rotation: 360, opacity:"0"}});
+
+    });
+
 });
 
-
 var incrementCounter = function(){
-
     var x = 0;
 
     setInterval(function(){
